8869dfec7756566932152dbf658806dca9667a04
Edwin <233282@ids.upchiapas.edu.mx>
2024-11-09 13:48:28 -0600

    secciones nuevas

--- src/app/pages/compra/components/compra/compra.component.ts
+++ src/app/pages/compra/components/compra/compra.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../header/component/header/header.component';
 
 @Component({
   selector: 'app-compra',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './compra.component.html',
   styleUrl: './compra.component.css'
 })
