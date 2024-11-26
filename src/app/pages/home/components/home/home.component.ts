77731ca70ae564b7bd8f2746d0fbaf9ef648e1e7
Edwin <233282@ids.upchiapas.edu.mx>
2024-11-26 01:11:05 -0600

    commit

--- src/app/pages/home/components/home/home.component.ts
+++ src/app/pages/home/components/home/home.component.ts
@@ -27,7 +27,6 @@ export class HomeComponent implements OnInit {
     { img: 'assets/producto4.png', name: 'Calculadora' }
   ];
   currentIndex: number = 0;
-  imageUrl: string = 'https://segundamanoup.s3.us-east-1.amazonaws.com/uploads/agura.jpg';
 
   constructor(private router: Router, private authService: AuthService) {}
 
